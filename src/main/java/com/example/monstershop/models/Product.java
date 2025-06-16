7c8a42764f799cd3921b5f08ec5c805de7085f99
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-06-16 15:21:04 +0200

    add product model constructor, getters and setters

---
+++ src/main/java/com/example/monstershop/models/Product.java
@@ -0,0 +1,81 @@
+package com.example.monstershop.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String imageUrl;
+    private double rating;
+    private int reviewCount;
+    private boolean featured;
+
+    public Product(){
+    }
+
+    public Product(String name, double price, String imageUrl, double rating, int reviewCount, boolean featured) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+        this.reviewCount = reviewCount;
+        this.featured = featured;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
+}
