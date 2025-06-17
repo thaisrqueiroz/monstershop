670ccc20d79f556f2292e18da72281503b4688cf
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-06-17 11:17:01 +0200

    add product request dto code

--- src/main/java/com/example/monstershop/dtos/product/ProductRequest.java
+++ src/main/java/com/example/monstershop/dtos/product/ProductRequest.java
@@ -1,1 +1,16 @@
+package com.example.monstershop.dtos.product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ProductRequest(
+        @NotBlank(message = "Name is required")
+        @Size(min = 5, max = 50, message = "Name must contain minimum 5 and maximo 50 characters")
+        String name,
+        double price,
+        String imageUrl,
+        double rating,
+        int reviewCount,
+        boolean featured
+) {
+}
