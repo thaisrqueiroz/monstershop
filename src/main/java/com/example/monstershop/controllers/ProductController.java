5fba819657c7f0ad46039ddd8f22bcfd6f53cda5
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-06-17 12:28:23 +0200

    add delete mapping product controller

--- src/main/java/com/example/monstershop/controllers/ProductController.java
+++ src/main/java/com/example/monstershop/controllers/ProductController.java
@@ -2,14 +2,12 @@ package com.example.monstershop.controllers;
 
 import com.example.monstershop.dtos.product.ProductRequest;
 import com.example.monstershop.dtos.product.ProductResponse;
+import com.example.monstershop.models.Product;
 import com.example.monstershop.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -28,5 +26,11 @@ public class ProductController {
     @PostMapping("/products")
     public ResponseEntity<ProductResponse> addProduct(@Valid @RequestBody ProductRequest productRequest) {
         return new ResponseEntity<>(productService.addProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
